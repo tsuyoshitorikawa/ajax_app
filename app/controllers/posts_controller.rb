fb7ec38b305bf5436ed9f11959ee5d2bf9e92e39
tsuyoshitorikawa <gangtulichuan@gmail.com>
2021-04-11 14:41:16 +0900

    メモ投稿機能のサーバーサイド実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,10 +1,12 @@
 class PostsController < ApplicationController
+
   def index
     @posts = Post.all.order(id: "DESC")
   end
+
   def create
-    Post.create(content: params[:content])
-    redirect_to action: :index
+    post = Post.create(content: params[:content], checked: false)
+    render json:{ post: post }
   end
   def checked
     binding.pry
